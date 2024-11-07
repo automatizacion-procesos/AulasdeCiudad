dd587d6471e31b600b5ae5d61eae16ffcccddb2c
automatizacion-procesos <115322950+automatizacion-procesos@users.noreply.github.com>
2024-11-07 14:44:03 -0500

    Update rpa_challenge_v2.py

--- RPA/soluciones rpa challenge/rpa_challenge_v2.py
+++ RPA/soluciones rpa challenge/rpa_challenge_v2.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
 
 
 #----------------------------CONFIGURACIONES----------------------------------#
@@ -40,8 +39,6 @@ XPATH_MENSAJE_RESULTADO = '//div[@class="message2"]'
 URL_RETO = 'https://rpachallenge.com/'
 
 
-# Instalamos el navegador de chrome
-chromedriver_autoinstaller.install() 
 
 # Definición de objeto que inicia y detiene el ChromeDriver
 s = Service()
